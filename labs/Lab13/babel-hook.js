4ef9b659ec7b50ca10657ee72e36a5ee57ec2821
fenago <socrates73@gmail.com>
2021-06-19 06:00:42 +0500

    lab 13 removed

--- labs/Lab13/babel-hook.js
+++
@@ -1,6 +0,0 @@
-require("@babel/register")({
-    "plugins": [
-      "require-context-hook", "react-loadable/babel", "dynamic-import-node"
-    ],
-    "presets": ["@babel/env","@babel/react"]
-});
